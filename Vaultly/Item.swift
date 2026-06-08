3cb1ad16f5b5924a3202f76ce0215851fed9f47f
Emmanuel <emanuel.mazari30@gmail.com>
2026-06-07 17:49:50 -0700

    Initial Commit

---
+++ Vaultly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vaultly
+//
+//  Created by Emmanuel Gallegos on 07/06/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VaultlyTests/VaultlyTests.swift
@@ -0,0 +1,18 @@
+//
+//  VaultlyTests.swift
+//  VaultlyTests
+//
+//  Created by Emmanuel Gallegos on 07/06/26.
+//
+
+import Testing
+
+struct VaultlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
